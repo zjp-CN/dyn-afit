68d27ef97cbf974be95c454a850863d785536a83
zjp <jiping_zhou@foxmail.com>
2024-10-17 22:21:50 +0800

    ok-dynosaur: dynamic dispatch: with boxing overhead twice

--- examples/ok-dynosaur.rs
+++ examples/ok-dynosaur.rs
@@ -4,12 +4,15 @@ use std::io; // pretend a Result is defined somewhere without std
 async fn main() {
     let mut file = _impl::File::new();
 
-    // static dispatch: no overhead
-    _ = dbg!(file.read(&mut []).await);
-
-    // dynamic dispatch: with boxing overhead
+    // dynamic dispatch: with boxing overhead once (in calling read on DynAsyncRead)
     let dyn_async_read = DynAsyncRead::from_mut(&mut file);
     _ = dbg!(call(dyn_async_read).await);
+
+    // dynamic dispatch: with boxing overhead twice in
+    // * creating a Boxed DynAsyncRead value
+    // * and calling read on DynAsyncRead
+    let mut box_dyn_async_read = DynAsyncRead::boxed(file);
+    _ = dbg!(call(&mut box_dyn_async_read).await);
 }
 
 #[dynosaur::dynosaur(DynAsyncRead)]
@@ -43,3 +46,83 @@ mod _impl {
         }
     }
 }
+
+// #[allow(async_fn_in_trait)]
+// pub trait AsyncRead {
+//     async fn read(&mut self, buf: &mut [u8]) -> io::Result<usize>;
+// }
+// mod _dynosaur_macro_dynasyncread {
+//     use super::*;
+//     #[allow(async_fn_in_trait)]
+//     pub trait ErasedAsyncRead {
+//         fn read<'life0, 'life1, 'dynosaur>(
+//             &'life0 mut self,
+//             buf: &'life1 mut [u8],
+//         ) -> ::core::pin::Pin<
+//             Box<dyn ::core::future::Future<Output = io::Result<usize>> + 'dynosaur>,
+//         >
+//         where
+//             'life0: 'dynosaur,
+//             'life1: 'dynosaur,
+//             Self: 'dynosaur;
+//     }
+//     impl<DYNOSAUR: AsyncRead> ErasedAsyncRead for DYNOSAUR {
+//         fn read<'life0, 'life1, 'dynosaur>(
+//             &'life0 mut self,
+//             buf: &'life1 mut [u8],
+//         ) -> ::core::pin::Pin<
+//             Box<dyn ::core::future::Future<Output = io::Result<usize>> + 'dynosaur>,
+//         >
+//         where
+//             'life0: 'dynosaur,
+//             'life1: 'dynosaur,
+//             Self: 'dynosaur,
+//         {
+//             Box::pin(<Self as AsyncRead>::read(self, buf))
+//         }
+//     }
+//     #[repr(transparent)]
+//     pub struct DynAsyncRead<'dynosaur_struct> {
+//         ptr: dyn ErasedAsyncRead + 'dynosaur_struct,
+//     }
+//     impl<'dynosaur_struct> AsyncRead for DynAsyncRead<'dynosaur_struct> {
+//         fn read(
+//             &mut self,
+//             buf: &mut [u8],
+//         ) -> impl ::core::future::Future<Output = io::Result<usize>> {
+//             let fut: ::core::pin::Pin<
+//                 Box<dyn ::core::future::Future<Output = io::Result<usize>> + '_>,
+//             > = self.ptr.read(buf);
+//             let fut: ::core::pin::Pin<
+//                 Box<dyn ::core::future::Future<Output = io::Result<usize>> + 'static>,
+//             > = unsafe { ::core::mem::transmute(fut) };
+//             fut
+//         }
+//     }
+//     impl<'dynosaur_struct> DynAsyncRead<'dynosaur_struct> {
+//         pub fn new(
+//             value: Box<impl AsyncRead + 'dynosaur_struct>,
+//         ) -> Box<DynAsyncRead<'dynosaur_struct>> {
+//             let value: Box<dyn ErasedAsyncRead + 'dynosaur_struct> = value;
+//             unsafe { ::core::mem::transmute(value) }
+//         }
+//         pub fn boxed(
+//             value: impl AsyncRead + 'dynosaur_struct,
+//         ) -> Box<DynAsyncRead<'dynosaur_struct>> {
+//             Self::new(Box::new(value))
+//         }
+//         pub fn from_ref(
+//             value: &(impl AsyncRead + 'dynosaur_struct),
+//         ) -> &DynAsyncRead<'dynosaur_struct> {
+//             let value: &(dyn ErasedAsyncRead + 'dynosaur_struct) = &*value;
+//             unsafe { ::core::mem::transmute(value) }
+//         }
+//         pub fn from_mut(
+//             value: &mut (impl AsyncRead + 'dynosaur_struct),
+//         ) -> &mut DynAsyncRead<'dynosaur_struct> {
+//             let value: &mut (dyn ErasedAsyncRead + 'dynosaur_struct) = &mut *value;
+//             unsafe { ::core::mem::transmute(value) }
+//         }
+//     }
+// }
+// use _dynosaur_macro_dynasyncread::DynAsyncRead;
