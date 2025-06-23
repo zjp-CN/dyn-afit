481548572156fe1640b6902f3b5982bce7f7631e
zjp <jiping_zhou@foxmail.com>
2025-06-23 14:52:58 +0800

    afidt-pin-init: stack alignment adjust and large future

--- afidt-pin-init/src/main.rs
+++ afidt-pin-init/src/main.rs
@@ -17,6 +17,17 @@ impl Async for A {
     }
 }
 
+struct B;
+impl Async for B {
+    #[dyn_init]
+    async fn foo(&self) {
+        let large_size = [0; 100];
+        async { println!("B") }.await;
+        // Use the large buffer across await point to make the future large.
+        _ = large_size;
+    }
+}
+
 fn main() {
     pollster::block_on(async {
         let a = A {};
@@ -26,40 +37,50 @@ fn main() {
 
         let ref_a: &dyn Async = &a;
         dynamic_dispatch(ref_a).await;
+
+        dynamic_dispatch(&B).await;
     })
 }
 
+const FUT_STACK_SIZE: usize = 64;
+
 async fn dynamic_dispatch(ref_a: &dyn Async) {
     let dyn_foo = ref_a.dyn_foo();
-    let layout = dbg!(dyn_foo.layout());
+    let layout = dyn_foo.layout();
+    let fut_size = layout.size();
 
-    if layout.size() > 16 {
-        // heap allocation if the future is too large
+    if fut_size > FUT_STACK_SIZE {
+        println!("Heap allocation as the future is too large.");
         Box::into_pin(Box::dyn_init(dyn_foo)).await;
     } else {
-        let mut stack = [0; 16];
+        let mut stack = [0u8; FUT_STACK_SIZE];
 
-        let slot = &mut stack as *mut _ as *mut ();
+        let start = &mut stack as *mut _ as *mut u8;
+        let end = start.wrapping_add(FUT_STACK_SIZE);
+        let slot = start.wrapping_add(start.align_offset(layout.align()));
+        let slot_end = slot.wrapping_add(fut_size);
 
-        let pin_dyn_fut = unsafe {
-            let meta = dyn_foo.init(slot).unwrap();
-            dbg!(meta);
-            let ptr_dyn_fut = ptr::from_raw_parts_mut::<dyn Future<Output = ()>>(&mut stack, meta);
+        // dbg!(start, slot, slot_end, end, fut_size, layout.align());
+        if !(start <= slot && slot_end <= end) {
+            println!("Heap allocation due to stack is not enough.");
+            Box::into_pin(Box::dyn_init(dyn_foo)).await;
+            return;
+        }
+
+        let pin_dyn_fut: Pin<&mut dyn Future<Output = ()>> = unsafe {
+            let meta = dyn_foo.init(slot as *mut ()).unwrap();
+            let ptr_dyn_fut = ptr::from_raw_parts_mut(&mut stack, meta);
             Pin::new_unchecked(&mut *ptr_dyn_fut)
         };
 
-        // no allocation if it's small enough
+        println!("Stack allocation. 🦀");
         pin_dyn_fut.await;
     }
 }
 
 // [OUTPUT]
 // foo!
-// [src/main.rs:38:18] dyn_foo.layout() = Layout {
-//     size: 16,
-//     align: 8 (1 << 3),
-// }
-// [src/main.rs:44:13] meta = DynMetadata(
-//     0x0000000000046358,
-// )
+// Stack allocation. 🦀
 // foo!
+// Heap allocation as the future is too large.
+// B
